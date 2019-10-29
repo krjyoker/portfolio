a7d51c0644002ae11cb54d6314b261ec3e64cf82
小野寺佑輝 <onodera_yuuki@onoderayuuakiranoMacBook-Pro.local>
2019-10-29 18:18:13 +0900

    10/29_1818

---
+++ onodera_yuki/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+ReactDOM.render(
+    <BrowserRouter>
+        <ToDo />
+        <Route exact path={'/'} component={Home}/>
+        <Route path={'/about'} component={About}/>
+        <Route path={'/work'} component={Work}/>
+        <Route path={'/contact'} component={Contact}/>
+    </BrowserRouter>
+    , document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
